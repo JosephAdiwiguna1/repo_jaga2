0ae73f8efd1efc0c72711201f5396bb31cb8e1bf
Benedict Wijaya <benwijaya03@gmail.com>
2025-01-08 19:46:00 +0700

    login + merge dengan joseph

---
+++ app/Models/Karyawan.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Karyawan extends Authenticatable
+{
+    use HasFactory;
+    protected $table = 'karyawan';
+
+    protected $fillable = [
+        'id_karyawan',
+        'departemen',
+        'jabatan',
+        'role',
+        'golongan',
+        'nama',
+        'alamat',
+        'email',
+        'password',
+        'no_telepon',
+    ];
+
+    protected $primaryKey = 'id_karyawan';
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+}
